2373c1c6c83a49dc4d628138373cc37598d11a67
javier.brea <javier.brea@gmail.com>
2021-02-17 19:16:23 +0100

    feat: add redirections. Use new index page. Add new links to header

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -61,7 +61,17 @@ module.exports = {
       items: [
         {
           to: "docs/get-started-intro",
-          label: "Docs",
+          label: "Get started",
+          position: "right",
+        },
+        {
+          to: "docs/configuration-options",
+          label: "Configuration",
+          position: "right",
+        },
+        {
+          to: "docs/api-mocks-server-api",
+          label: "API",
           position: "right",
         },
         {
